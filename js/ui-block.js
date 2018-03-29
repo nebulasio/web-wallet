f4cb4389114099fd159e1469dd836d3712ccff74
Yogi Xun <xun@nebulas.io>
2018-03-29 17:16:16 +0800

    rpc bugfix

--- js/ui-block.js
+++ js/ui-block.js
@@ -134,8 +134,9 @@ var uiBlock = function () {
         function logoMain(selector) {
             var i, len,
                 list = [
-                    { chainId: 1, name: "mainnet.nebulas.io", url: "http://mainnet.nebulas.io/" },
-                    { chainId: 1001, name: "testnet.nebulas.io", url: "https://testnet.nebulas.io/" }
+                    { chainId: 1, name: "mainnet.nebulas.io", url: "http://mainnet.nebulas.io" },
+                    { chainId: 1001, name: "testnet.nebulas.io", url: "https://testnet.nebulas.io" },
+                    { chainId: 1003, name: "develop", url: "http://13.57.120.136:8685" }
                 ],
                 apiPrefix, sApiButtons, sApiText,
                 lang, langs, sLangButtons;
